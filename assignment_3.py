54b04a6abc1733ba25ab83f0d639c7d1dbea75c4
Xerok <67823102+MrRasuloov@users.noreply.github.com>
2021-04-28 14:42:01 +0500

    Add files via upload

---
+++ assignment_3.py
@@ -0,0 +1,44 @@
+def add ( a, b ):
+    print ( f"\nAdding {a}+{b}" )
+    return a + b
+
+
+add ( 10, 30 )
+
+
+def say_hello ( your_name ):
+    print ( f"\nHello {your_name}!" )
+
+
+say_hello ( "Xondamir" )
+
+
+def p_lang ( smth ):
+    print ( f"\nI am learning {smth}." )
+
+
+p_lang ( "Python" )
+
+
+def message ( smth2 ):
+    print ( f"\nThis is a little bit confusing creating {smth2} :(." )
+
+
+message ( "functions" )
+
+
+def adding ( a, b ):
+    a = 10
+    b = 40
+    print ( f"\nadd ({a}+{b})")
+
+adding ( 10, 40 )
+
+def weather(weather_condition):
+    print(f"\nWe will go out if it will {weather_condition}!")
+weather("sunny")
+
+def price(price):
+    print(f"\nI will buy it if it's no more than {price}")
+price("10$")
+
